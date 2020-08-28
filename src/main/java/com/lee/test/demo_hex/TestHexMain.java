f45b18335c8ecd2ba802866d0525669ed6f38aff
wanghongen <wanghongen@e-sscard.com>
2020-08-28 17:35:31 +0800

    sm_sign

--- src/main/java/com/lee/test/demo_hex/TestHexMain.java
+++ src/main/java/com/lee/test/demo_hex/TestHexMain.java
@@ -3,6 +3,7 @@ package com.lee.test.demo_hex;
 import com.lee.utils.ConverterUtil;
 import com.lee.utils.SM234Util;
 import com.lee.utils.sm2.SM2KeyVO;
+import com.lee.utils.sm2.SM2SignVO;
 
 public class TestHexMain {
 
@@ -12,19 +13,27 @@ public class TestHexMain {
         String publicKey = "049975A7BA7FC6BDBCD32A638F5D90047E011D61758A1C061C6999812A9C0597753B7B02FB9178B364DCC346EC995B7AE6C8E5BAD8C44505FF17BFF79A6655F632";
         String privateKey = "0891CD3D5B775EFBCAF6E516721CB0E8ECE086032C94AB335E121DE6847323B1";
 
-        //随机生成密钥对（公钥、私钥），对内容做加解密
-        sm2_00_enc_dec("我是一段测试aaaa王红恩");
+        String text = "我是一段测试aaaa王红恩";
 
-        //使用指定的密钥对（公钥、私钥），对内容做加解密
-        sm2_01_enc_dec(publicKey, privateKey, "我是一段测试aaaa王红恩");
+        //sm2非对称加密----随机生成密钥对（公钥、私钥），对内容做加解密
+        //sm2_00_enc_dec(text);
 
+        //sm2非对称加密----使用指定的密钥对（公钥、私钥），对内容做加解密
+        //sm2_01_enc_dec(publicKey, privateKey, text);
 
+        //sm2签名
+        sm2_sign(text);
+
+        //sm4对称加密----
+        //sm4_00_enc_dec("我是一段测试aaaa王红恩");
 
 
     }
 
     /**
+     * sm2非对称加密
      * 随机生成密钥对（公钥、私钥），对内容做加解密
+     *
      * @param text
      * @throws Exception
      */
@@ -49,7 +58,9 @@ public class TestHexMain {
     }
 
     /**
+     * sm2非对称加密
      * 使用指定的密钥对（公钥、私钥），对内容做加解密
+     *
      * @param publicKey
      * @param privateKey
      * @param text
@@ -68,5 +79,88 @@ public class TestHexMain {
         String SM2Dec = SM234Util.SM2Dec(privateKey, SM2Enc);
         System.out.println("解密:" + SM2Dec);
         System.out.println("--测试加密结束--");
+    }
+
+    /**
+     * SM2签名
+     *
+     * @param text
+     * @throws Exception
+     */
+    public static void sm2_sign(String text) throws Exception {
+        String src = text;
+        System.out.println("--测试SM2签名--");
+        System.out.println("原文hex:" + ConverterUtil.byteToHex(src.getBytes()));
+        String s5 = ConverterUtil.byteToHex(src.getBytes());
+
+        System.out.println("--产生SM2秘钥--:");
+        SM2KeyVO sm2KeyVO = SM234Util.generateSM2Key();
+        System.out.println("公钥:" + sm2KeyVO.getPubHexInSoft());
+        System.out.println("私钥:" + sm2KeyVO.getPriHexInSoft());
+
+        System.out.println("签名测试开始:");
+        SM2SignVO sign = SM234Util.genSM2Signature(sm2KeyVO.getPriHexInSoft(), s5);
+        System.out.println("软加密签名结果:" + sign.getSm2_signForSoft());
+        System.out.println("加密机签名结果:" + sign.getSm2_signForHard());
+        //System.out.println("转签名测试:"+SM2SignHardToSoft(sign.getSm2_signForHard()));
+        System.out.println("验签1,软件加密方式:");
+        boolean b = SM234Util.verifySM2Signature(sm2KeyVO.getPubHexInSoft(), s5, sign.getSm2_signForSoft());
+        System.out.println("软件加密方式验签结果:" + b);
+        System.out.println("验签2,硬件加密方式:");
+        String sm2_signForHard = sign.getSm2_signForHard();
+        System.out.println("签名R:" + sign.sign_r);
+        System.out.println("签名S:" + sign.sign_s);
+        //System.out.println("硬:"+sm2_signForHard);
+        b = SM234Util.verifySM2Signature(sm2KeyVO.getPubHexInSoft(), s5, SM234Util.SM2SignHardToSoft(sign.getSm2_signForHard()));
+        System.out.println("硬件加密方式验签结果:" + b);
+        if (!b) {
+            throw new RuntimeException();
+        }
+        System.out.println("--签名测试结束--");
+    }
+
+    /**
+     * sm3摘要加密hash
+     *
+     * @param text
+     */
+    public static void sm3(String text) {
+        String src = text;
+        System.out.println("--SM3摘要测试--");
+        String s = SM234Util.generateSM3HASH(src);
+        System.out.println("hash:" + s);
+        System.out.println("--SM3摘要结束--");
+
+    }
+
+    /**
+     * sm4对称加密
+     * 随机生成密钥对（公钥、私钥），对内容做加解密
+     *
+     * @param text
+     * @throws Exception
+     */
+    public static void sm4_00_enc_dec(String text) throws Exception {
+
+        String src = text;
+
+        System.out.println("--生成SM4秘钥--");
+        String sm4Key = SM234Util.generateSM4Key();
+
+        System.out.println("sm4Key:" + sm4Key);
+        System.out.println("--生成SM4结束--");
+        System.out.println("--SM4的CBC加密--");
+        String s1 = SM234Util.SM4EncForCBC(sm4Key, src);
+        System.out.println("密文:" + s1);
+        System.out.println("CBC解密");
+        String s2 = SM234Util.SM4DecForCBC(sm4Key, s1);
+        System.out.println("解密结果:" + s2);
+
+        System.out.println("--ECB加密--");
+        String s3 = SM234Util.SM4EncForECB(sm4Key, src);
+        System.out.println("ECB密文:" + s3);
+        System.out.println("ECB解密");
+        String s4 = SM234Util.SM4DecForECB(sm4Key, s3);
+        System.out.println("ECB解密结果:" + s4);
     }
 }
--- src/main/java/com/lee/utils/SM234Util.java
+++ src/main/java/com/lee/utils/SM234Util.java
@@ -2,10 +2,20 @@ package com.lee.utils;
 
 import com.lee.utils.sm2.SM2EncDecUtils;
 import com.lee.utils.sm2.SM2KeyVO;
+import com.lee.utils.sm2.SM2SignVO;
+import com.lee.utils.sm2.SM2SignVerUtils;
+import com.lee.utils.sm4.SM4Utils;
+import org.bouncycastle.asn1.ASN1EncodableVector;
+import org.bouncycastle.asn1.ASN1Integer;
+import org.bouncycastle.asn1.DERSequence;
+import org.bouncycastle.crypto.digests.SM3Digest;
+import org.bouncycastle.util.encoders.Hex;
 
 import java.io.IOException;
+import java.util.UUID;
 
 public class SM234Util {
+
     public static final String SM2PubHardKeyHead = "3059301306072A8648CE3D020106082A811CCF5501822D034200";
 
     //产生非对称秘钥
@@ -18,16 +28,113 @@ public class SM234Util {
     public static String SM2Enc(String pubKey, String src) throws IOException {
         String encrypt = SM2EncDecUtils.encrypt(ConverterUtil.hexStringToBytes(pubKey), src.getBytes());
         //删除04
-        encrypt=encrypt.substring(2,encrypt.length());
+        encrypt = encrypt.substring(2, encrypt.length());
         return encrypt;
     }
 
     //私钥解密
     public static String SM2Dec(String priKey, String encryptedData) throws IOException {
         //填充04
-        encryptedData="04"+encryptedData;
+        encryptedData = "04" + encryptedData;
         byte[] decrypt = SM2EncDecUtils.decrypt(ConverterUtil.hexStringToBytes(priKey), ConverterUtil.hexStringToBytes(encryptedData));
         return new String(decrypt);
+    }
+
+
+    //私钥签名,参数二:原串必须是hex!!!!因为是直接用于计算签名的,可能是SM3串,也可能是普通串转Hex
+    public static SM2SignVO genSM2Signature(String priKey, String sourceData) throws Exception {
+        SM2SignVO sign = SM2SignVerUtils.Sign2SM2(ConverterUtil.hexToByte(priKey), ConverterUtil.hexToByte(sourceData));
+        return sign;
+    }
+
+    //公钥验签,参数二:原串必须是hex!!!!因为是直接用于计算签名的,可能是SM3串,也可能是普通串转Hex
+    public static boolean verifySM2Signature(String pubKey, String sourceData, String hardSign) {
+        SM2SignVO verify = SM2SignVerUtils.VerifySignSM2(ConverterUtil.hexStringToBytes(pubKey), ConverterUtil.hexToByte(sourceData), ConverterUtil.hexToByte(hardSign));
+        return verify.isVerify();
+    }
+
+    //SM2签名Hard转soft
+    public static String SM2SignHardToSoft(String hardSign) {
+        byte[] bytes = ConverterUtil.hexToByte(hardSign);
+        byte[] r = new byte[bytes.length / 2];
+        byte[] s = new byte[bytes.length / 2];
+        System.arraycopy(bytes, 0, r, 0, bytes.length / 2);
+        System.arraycopy(bytes, bytes.length / 2, s, 0, bytes.length / 2);
+        ASN1Integer d_r = new ASN1Integer(ConverterUtil.byteConvertInteger(r));
+        ASN1Integer d_s = new ASN1Integer(ConverterUtil.byteConvertInteger(s));
+        ASN1EncodableVector v2 = new ASN1EncodableVector();
+        v2.add(d_r);
+        v2.add(d_s);
+        DERSequence sign = new DERSequence(v2);
+
+        String result = null;
+        try {
+            result = ConverterUtil.byteToHex(sign.getEncoded());
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        //SM2加密机转软加密编码格式
+        //return SM2SignHardKeyHead+hardSign.substring(0, hardSign.length()/2)+SM2SignHardKeyMid+hardSign.substring(hardSign.length()/2);
+        return result;
+    }
+
+
+    //摘要计算
+    public static String generateSM3HASH(String src) {
+        byte[] md = new byte[32];
+        byte[] msg1 = src.getBytes();
+        //System.out.println(Util.byteToHex(msg1));
+        SM3Digest sm3 = new SM3Digest();
+        sm3.update(msg1, 0, msg1.length);
+        sm3.doFinal(md, 0);
+        String s = new String(Hex.encode(md));
+        return s.toUpperCase();
+    }
+
+
+    //产生对称秘钥
+    public static String generateSM4Key() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+
+
+    //对称秘钥加密(CBC)
+    public static String SM4EncForCBC(String key, String text) {
+        SM4Utils sm4 = new SM4Utils();
+        sm4.secretKey = key;
+        sm4.hexString = true;
+        sm4.iv = "31313131313131313131313131313131";
+        String cipherText = sm4.encryptData_CBC(text);
+        return cipherText;
+    }
+
+    //对称秘钥解密(CBC)
+    public static String SM4DecForCBC(String key, String text) {
+        SM4Utils sm4 = new SM4Utils();
+        sm4.secretKey = key;
+        sm4.hexString = true;
+        sm4.iv = "31313131313131313131313131313131";
+        String plainText = sm4.decryptData_CBC(text);
+        return plainText;
+    }
+
+    //对称秘钥加密(ECB)
+    public static String SM4EncForECB(String key, String text) {
+        SM4Utils sm4 = new SM4Utils();
+        sm4.secretKey = key;
+        sm4.hexString = true;
+        String cipherText = sm4.encryptData_ECB(text);
+        return cipherText;
+    }
+
+    //对称秘钥解密(ECB)
+    public static String SM4DecForECB(String key, String text) {
+        SM4Utils sm4 = new SM4Utils();
+        sm4.secretKey = key;
+        sm4.hexString = true;
+        String plainText = sm4.decryptData_ECB(text);
+        return plainText;
     }
 
 }
